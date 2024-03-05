5a7f57ed99152b3596c319d6d10dbf189d75992a
Aurelian <aurelian_reghin@yahoo.com>
2024-03-05 11:12:40 +0200

    update

--- src/js/model/modelDataStorage.js
+++ src/js/model/modelDataStorage.js
@@ -853,10 +853,36 @@ module.exports = router;
       ],
     },
     {
-      sectionTitle: 'Serving Files Statically',
+      sectionTitle: 'Serving Files Statically with <code>express.static</code>',
+      highlights: {
+        highlight1: ['<code>express.static</code>'],
+      },
       sectionSource: '',
       tooltips: [
-        `<pre><code>
+        `<p>To serve static files such as images, CSS files, and JavaScript files, use the <code>express.static</code> built-in middleware function in Express.js.</p>
+        <p>The function signature is: <code>express.static(root, [options])</code>. The <code>root</code> argument specifies <i>the root directory from which to serve static assets</i>.</p>
+        <p>For example, use the following code to serve images, CSS files, and JavaScript files in a directory named public:</p>
+        <pre><code>
+app.use(express.static('public'))
+        </code></pre>
+        <p>Now, you can load the files that are in the public directory:</p>
+        <pre><code>
+http://localhost:3000/images/kitten.jpg
+http://localhost:3000/css/style.css
+http://localhost:3000/js/app.js
+http://localhost:3000/images/bg.png
+http://localhost:3000/hello.html
+        </code></pre>
+        <p><i>NOTE: Express.js looks up the files relative to the static directory, so the name of the static directory is not part of the URL.</i></p>
+        <p>To use multiple static assets directories, call the express.static middleware function multiple times:</p>
+        <pre><code>
+app.use(express.static('public'))
+app.use(express.static('files'))
+        </code></pre>
+        <p><i>NOTE: Express.js looks up the files in the order in which you set the static directories with the <code>express.static</code> middleware function.</i></p>
+        `,
+        `<h3>Another example</h3>
+        <pre><code>
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
